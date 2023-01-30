ca9c22744acf1a478acc583d8aafda2c33766c4f
postforty <75767073+postforty@users.noreply.github.com>
2023-01-30 23:10:18 +0900

    [UPDATE] 실시간 환율 계산기 - input 추가

--- main7-3_currency.py
+++ main7-3_currency.py
@@ -20,17 +20,34 @@ def get_exchange_rate():
     # content1 = content.find('div', 'rate_table_bx _table')
     containers = content.select('div > table > tbody > tr > td > span')
 
-    country = ['미국', '일본', '유럽연합', '중국', '영국', '호주', '캐나다', '뉴질랜드']
-    currency = []
+    country = ['미국 USD', '일본 JPY 100', '유럽연합 EUR', '중국 CNY', '영국 GBP', '호주 AUD', '캐나다 CAD', '뉴질랜드 NZD']
+    currency_str = []
+    currency_float = []
 
     for idx, val in enumerate(containers):
         if idx % 4 == 0:
             result = re.sub('<span>|</span>','', str(val))
-            currency.append(result)
+            currency_str.append(result)
+            currency_float.append(float(re.sub(',','', result)))
     
-    for idx, val in enumerate(country):
-        print(val, end=' ')
-        print(currency[idx])
+    print('='*30)
+    print("{0:|^25}".format(' 환율 변환기 '))
+    print('='*30)
 
+    for idx, val in enumerate(country):
+        print(f'{idx + 1})', val, end=': ')
+        print(currency_str[idx], 'KRW')
+
+    print('='*30)
+    
+    select_no = int(input('변환을 원하는 통화를 선택해 주세요: ')) - 1
+    amount = int(input('변환하고자하는 금액을 입력해 주세요: '))
+
+    # JPY 100 분기
+    if country[select_no][-3:] == '100':
+        print(f'{"{:0,.2f}".format(amount)} {country[select_no]} 변환 결과: ', '{:0,.2f}'.format(currency_float[select_no] * amount / 100), 'KRW')
+    else:
+        print(f'{"{:0,.2f}".format(amount)} {country[select_no]} 변환 결과: ', '{:0,.2f}'.format(currency_float[select_no] * amount), 'KRW')
+    
 # get_exchange_rate('usd', 'krw')
 get_exchange_rate()
