8ef944c71ed26def31127ab552b4900b2893b2d1
postforty <75767073+postforty@users.noreply.github.com>
2023-01-31 22:38:55 +0900

    01_숫자 맞추기 게임 #2

--- main1-1_number_quiz.py
+++ main1-1_number_quiz.py
@@ -2,4 +2,19 @@ import random
 
 ran_num = random.randint(1, 100)
 
-print(ran_num)
+# print(ran_num)
+
+count = 1
+
+while True:
+    num = int(input("1~100 사이의 숫자를 입력하세요: "))
+
+    if num > ran_num:
+        print("다운")
+    elif num < ran_num:
+        print("업")
+    elif num == ran_num:
+        print(f'정답입니다. {count}회 만에 맞췄습니다.')
+        break
+
+    count += 1
