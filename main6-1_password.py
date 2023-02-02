71e98a8e784cad804990d6c551e21814045f0ed4
postforty <75767073+postforty@users.noreply.github.com>
2023-02-02 21:15:48 +0900

    06_압축파일 암호 푸는 프로그램 #2

--- main6-1_password.py
+++ main6-1_password.py
@@ -1,9 +1,18 @@
 import itertools
+import zipfile
 
 passwd_str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-for i in range(1, 4):
+zFile = zipfile.ZipFile('secret.zip')
+
+for i in range(4, 5):
     to_attempt = itertools.product(passwd_str, repeat = i)
     for attempt in to_attempt:
         passwd = ''.join(attempt)
         print(passwd)
+        try:
+            zFile.extractall(pwd = passwd.encode())
+            print(f"비밀번호는 {passwd}입니다.")
+            break
+        except:
+            pass
