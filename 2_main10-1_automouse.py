4208950ad001565436a45617799f4bdc40420878
postforty <75767073+postforty@users.noreply.github.com>
2023-02-04 22:24:44 +0900

    2-1_오토마우스를 활용한 웹페이지 자동화 #2

--- 2_main10-1_automouse.py
+++ 2_main10-1_automouse.py
@@ -1,7 +1,19 @@
 # pip install pyautogui
 import pyautogui
 import time
+import pyperclip
 
-while True:
-    print(pyautogui.position())
-    time.sleep(0.1)
+# while True:
+#     print(pyautogui.position())
+#     time.sleep(0.1)
+
+pyautogui.moveTo(1280,155,0.2)
+pyautogui.click()
+time.sleep(0.5)
+
+pyperclip.copy("부산 날씨")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(0.5)
+
+pyautogui.write(["enter"])
+time.sleep(1)
