abb81ad69d5c41bb7ca7d0a2a80cc3188d2a7965
postforty <75767073+postforty@users.noreply.github.com>
2023-02-04 12:34:01 +0900

    1강 뇌말랑 퀴즈 - 로또 번호 생성 - random 모듈

---
+++ algorithm/1_random.py
@@ -0,0 +1,18 @@
+import random
+
+result =  []
+while True:
+    num = random.randint(1, 46)
+    if len(result) == 0:
+        result.append(num)
+    try:
+        result.index(num)
+    except:
+        result.append(num)
+    else:
+        pass
+    finally:
+        if len(result) == 6:
+            break
+
+print(result)
