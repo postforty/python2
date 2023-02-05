964dd8b173bc7ae71795420e196b7d29c717910e
postforty <75767073+postforty@users.noreply.github.com>
2023-02-05 09:59:00 +0900

    2-2_엑셀 정보로 수료증 문서 자동 생성 #2

---
+++ 2_2_1_main12-3_certificate.py
@@ -0,0 +1,72 @@
+# pip install python-docx
+import docx
+from docx.oxml.ns import qn
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+doc = docx.Document('certificate_form.docx')
+
+style = doc.styles['Normal']
+style.font.name = '나눔고딕'
+style._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+style.font.size = docx.shared.Pt(12)
+
+para = doc.add_paragraph()
+# run = para.add_run('\n\n')
+run = para.add_run(' 제 2020-001 호\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n')
+run = para.add_run('수 료 증')
+run.font.name = '나눔고딕'
+run.bold = True
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(40)
+para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n')
+run = para.add_run(' 성   명: 김일남\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+run = para.add_run(' 생년월일: 1950.01.01\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+run = para.add_run(' 교육과정: Python 1, Python 2\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+run = para.add_run(' 교육날짜: 2022.12.17 ~ 2023.2.26\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n')
+run = para.add_run(' 위 사람은 Python 1, Python 2 교육과정을\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+run = para.add_run(' 탁월하게 이수하였으므로 이 증서를 수여 합니다.\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n\n')
+run = para.add_run('코리아 IT 아카데미')
+run.font.name = '나눔고딕'
+run.bold = True
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+doc.save('certificate_result.docx')
