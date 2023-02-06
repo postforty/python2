70299a6b144c46f94d8dcadd257606e8ec61fbd3
postforty <75767073+postforty@users.noreply.github.com>
2023-02-06 23:35:32 +0900

    3-1_구글 이미지 크롤링

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,9 @@
- '맑은 고딕'
-# style._element.rPr.rFont
+uest
+
+# count = 0
+# for idx, value in enumerate(links):
+#     url = value
+#     urllib.request.urlretrieve(url, f'./crawling_img/bada_{idx}.jpg')
+#     count += 1
+
+# print(f'{count}개 이미지 다운로드 완료')
