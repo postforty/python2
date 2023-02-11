033fe93e20745c05d84d61d8d659de96428973c3
postforty <75767073+postforty@users.noreply.github.com>
2023-02-11 21:16:13 +0900

    6_1_1_chat

---
+++ 6_1_1_chat_server.py
@@ -0,0 +1,39 @@
+# https://foxtrotin.tistory.com/272
+from socket import *
+import threading
+import time
+
+
+def send(sock):
+    while True:
+        sendData = input('>>>')
+        sock.send(sendData.encode('utf-8'))
+
+
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        print('상대방 :', recvData.decode('utf-8'))
+
+
+port = 8081
+
+serverSock = socket(AF_INET, SOCK_STREAM)
+serverSock.bind(('', port))
+serverSock.listen(1)
+
+print('%d번 포트로 접속 대기중...'%port)
+
+connectionSock, addr = serverSock.accept()
+
+print(str(addr), '에서 접속되었습니다.')
+
+sender = threading.Thread(target=send, args=(connectionSock,)) #send함수를 인자를 넣어 쓰레드 생성
+receiver = threading.Thread(target=receive, args=(connectionSock,)) #receive함수를 인자를 넣어 쓰레드 생성
+
+sender.start()
+receiver.start()
+
+while True: #프로그램을 계속 실행한다
+    time.sleep(1) #1초 쉰다
+    pass
