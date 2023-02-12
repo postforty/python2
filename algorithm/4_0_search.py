7800d15f99af8907452c0919c8a66c6e6377f565
postforty <75767073+postforty@users.noreply.github.com>
2023-02-12 22:38:18 +0900

    질문 관련 코드

--- algorithm/4_0_search.py
+++ algorithm/4_0_search.py
@@ -14,16 +14,27 @@
 # 입력시 공백으로 구분
 # 손님이 요청한 부품 번호의 순서대로 부품을 확인해 부품이 있으면 yes, 없으면 no 출력
 
-n = int(input())
+n = int(input("가게에 있는 부품 수? "))
 
-items = set(map(int, input().split()))
+items = set(map(int, input(f"가게이 있는 {n}개의 부품 번호? ").split()))
 
-m = int(input())
+m = int(input("손님이 요청한 부품 수? "))
 
-search_items = list(map(int, input().split()))
+search_items = list(map(int, input(f"손님이 요청한 {m}개의 부품 번호? ").split()))
 
 for i in search_items:
     if i in items:
         print('yes', end=' ')
     else:
         print('no', end=' ')
+
+
+# 수업 중 나온 질문
+# while 사용하지 않고 n개 만큼 입력 받는 코드
+n = int(input("가게에 있는 부품 수? "))
+
+def input_nums():
+    items = list(filter((lambda x: len(x) == n), [list(map(int, input().split()))]))
+    return items[0] if items else input_nums()
+
+print(f"가게이 있는 {n}개의 부품 번호? ", input_nums())
