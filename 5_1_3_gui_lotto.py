ffc5234b4ae0286a6fa810f46db2bde02ad0b19c
postforty <75767073+postforty@users.noreply.github.com>
2023-02-13 20:02:58 +0900

    [UPDATE] 5_1_로또 번호 생성기 GUI

---
+++ 5_1_3_gui_lotto.py
@@ -0,0 +1,43 @@
+import tkinter
+import tkinter.font
+import random
+
+lotto_num = range(1, 46)
+
+def buttonClick():
+    for i in range(5):
+        lottoPick = map(str, random.sample(lotto_num, 6))
+        lottoPick = ','.join(lottoPick)
+        lottoPick = f'{i+1}회: {lottoPick}'
+        print(lottoPick)
+        listbox.insert(i, lottoPick)
+    listbox.pack()
+
+window = tkinter.Tk()
+window.title("lotto")
+window.resizable(False, False)
+window.geometry("400x200+800+300") # 가로세로의 크기, +800+300은 초기 위치
+
+# 창 중앙 위치
+# w = 400
+# h = 150
+# sw = window.winfo_screenwidth()
+# sh = window.winfo_screenheight()
+# x = (sw - w)/2
+# y = (sh - h)/2
+# window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+button = tkinter.Button(window, overrelief="solid", text='번호확인', width=15, command=buttonClick, repeatdelay=1000, repeatinterval=500) # 마우스를 누르고 있을때 delay, delay 후 interval
+button.pack()
+
+font = tkinter.font.Font(size = 20)
+listbox = tkinter.Listbox(window, selectmode='extended', height=5, font=font)
+listbox.insert(0, "1회:")
+listbox.insert(1, "2회:")
+listbox.insert(2, "3회:")
+listbox.insert(3, "4회:")
+listbox.insert(4, "5회:")
+# listbox.delete(1, 2)
+listbox.pack()
+
+window.mainloop()
