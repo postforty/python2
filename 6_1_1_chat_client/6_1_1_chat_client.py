dd77b03c1faac979515cecc9af6baf24890556c9
postforty <75767073+postforty@users.noreply.github.com>
2023-02-15 22:49:39 +0900

    6_1_채팅_서버

--- 6_1_1_chat_client/6_1_1_chat_client.py
+++ 6_1_1_chat_client/6_1_1_chat_client.py
@@ -2,7 +2,7 @@ import socket
 from _thread import *
 
 HOST = '127.0.0.1'
-PORT = 9999
+PORT = 8081
 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
--- 6_1_1_chat_server.py
+++ 6_1_1_chat_server.py
@@ -4,18 +4,23 @@ from socket import *
 import threading
 import time
 
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
+ui_path = 'chat_server.ui'
+from_class = uic.loadUiType(ui_path)[0]
+
+# 채팅 서버
 def send(sock):
     while True:
         sendData = input('>>>')
         sock.send(sendData.encode('utf-8'))
 
-
 def receive(sock):
     while True:
         recvData = sock.recv(1024)
         print('상대방 :', recvData.decode('utf-8'))
-
 
 port = 8081
 
@@ -35,6 +40,26 @@ receiver = threading.Thread(target=receive, args=(connectionSock,)) #receive함�
 sender.start()
 receiver.start()
 
-while True: #프로그램을 계속 실행한다
-    time.sleep(1) #1초 쉰다
-    pass
+# while True: #프로그램을 계속 실행한다
+#     time.sleep(1) #1초 쉰다
+#     pass
+
+class WindowClass(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('채팅 서버')
+        self.btn_send.clicked.connect(self.btn_clicked)
+
+    def btn_clicked(self):
+        btn_value = self.input_msg.text()
+        self.msg_box.append(btn_value)
+        self.input_msg.clear()
+        print(btn_value)
+
+if __name__=="__main__":
+    app = QApplication(sys.argv) # sys.argv는 python으로 실행한 시스템 argument(Command Line Argument)를 확인
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
+
