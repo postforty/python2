2b50063332d5a7a4b4a0488759c0c89ca59ba03f
postforty <75767073+postforty@users.noreply.github.com>
2023-04-01 23:43:11 +0900

    Create 3_0_crawling_bs4.py

---
+++ 3_0_crawling_bs4.py
@@ -0,0 +1,23 @@
+# pip install BeautifulSoup4
+# pip install requests
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bkEw&pkid=68&os=5664043&qvt=0&query=%EC%98%81%ED%99%94%20%EA%B8%B0%EC%83%9D%EC%B6%A9%20%EB%A6%AC%EB%B7%B0'
+param = {'code': 161967}
+response = requests.get(url, params=param)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+# print(soup)
+
+review_list = soup.find_all('span', class_='this_text')
+
+# print(review_list)
+
+for review in review_list:
+    result = re.sub('<span class="this_text">|</span>','', str(review))
+    result = re.sub('&lt;','<', str(result))
+    result = re.sub('&gt;','>', str(result))
+    print(result)
