1688b0df32b92a646c7e915342c8a73028b715e4
postforty <75767073+postforty@users.noreply.github.com>
2023-04-15 07:43:44 +0900

    [ADD] bootstrap 까지

--- projects/mysite/myboard/views.py
+++ projects/mysite/myboard/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
+from django.utils import timezone
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -10,3 +11,9 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'myboard/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    print(question)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('myboard:detail', question_id=question.id)
