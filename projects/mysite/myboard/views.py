3714d064ba3c2dcc7e7e70e11984816bda1f4fc6
postforty <75767073+postforty@users.noreply.github.com>
2023-04-16 07:41:55 +0900

    [ADD] 질문 수정 삭제

--- projects/mysite/myboard/views.py
+++ projects/mysite/myboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm
+from django.contrib import messages
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -31,3 +32,22 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'myboard/question_form.html', context)
+
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('myboard:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'myboard/question_form.html', context)
+
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.delete()
+    return redirect('myboard:index')
