44889bc6907b273c9ca1646988413b75b21002fe
postforty <75767073+postforty@users.noreply.github.com>
2023-05-27 11:50:11 +0900

    [UPDATE] 자동화 프로그램 만들기

--- 2_1_1_automouse.py
+++ 2_1_1_automouse.py
@@ -5,27 +5,30 @@ import time
 import pyperclip
 
 # 1차 - 좌표 확인
-while True:
-    print(pyautogui.position())
-    time.sleep(0.1)
+# while True:
+#     print(pyautogui.position())
+#     time.sleep(0.1)
 
 # 2차 - 부산 날씨 이동
-# pyautogui.moveTo(318,150,0.2)
-# pyautogui.click()
-# time.sleep(0.5)
+pyautogui.moveTo(318, 150, 0.2)
+pyautogui.click()
+time.sleep(0.5)
 
-# pyperclip.copy("부산 날씨")
-# pyautogui.hotkey("ctrl", "v")
-# time.sleep(0.5)
+pyperclip.copy("부산 날씨")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(0.5)
 
 # pyautogui.write(["enter"])
-# time.sleep(1)
+pyautogui.press("enter")
+time.sleep(1)
 
-# # 3차 - 화면 캡쳐(52, 333 / 680, 620)
+# 3차 - 화면 캡쳐(52, 333 / 680, 620)
 
-# start_x = 52
-# start_y = 333
-# end_x = 680
-# end_y =620
+start_x = 52
+start_y = 333
+end_x = 680
+end_y = 620
 
-# pyautogui.screenshot(r'auto_img.png', region=(start_x, start_y, end_x-start_x, end_y-start_y))
+pyautogui.screenshot(
+    r"auto_img.png", region=(start_x, start_y, end_x - start_x, end_y - start_y)
+)
--- 2_1_2_automouse_up.py
+++ 2_1_2_automouse_up.py
@@ -2,7 +2,8 @@ import pyautogui
 import time
 import pyperclip
 
-weather = ["서울 날씨", "부산 날씨", "양산 날씨", "김해 날씨", "강원도 날씨", "제주도 날씨"]
+# weather = ["서울 날씨", "부산 날씨", "양산 날씨", "김해 날씨", "강원도 날씨", "제주도 날씨"]
+weather = ["서울 날씨"]
 
 addr_x = 260
 addr_y = 60
@@ -11,7 +12,7 @@ search_y = 150
 start_x = 52
 start_y = 333
 end_x = 680
-end_y =620
+end_y = 620
 
 # 1차 - 좌표 확인
 # while True:
@@ -19,9 +20,11 @@ end_y =620
 #     time.sleep(0.1)
 
 for w in weather:
-    pyautogui.moveTo(addr_x, addr_y, 0.2)
-    pyautogui.click()
-    time.sleep(0.5)
+    # pyautogui.moveTo(addr_x, addr_y, 0.2)
+    # pyautogui.click()
+    # time.sleep(0.5)
+    pyautogui.hotkey("alt", "tab")
+    pyautogui.hotkey("alt", "d")
     pyautogui.write("www.naver.com", interval=0.1)
     pyautogui.write(["enter"])
     time.sleep(1)
@@ -34,4 +37,6 @@ for w in weather:
     pyautogui.write(["enter"])
     time.sleep(1)
 
-    pyautogui.screenshot(f'auto_img_{w}.png', region=(start_x, start_y, end_x-start_x, end_y-start_y))
+    pyautogui.screenshot(
+        f"auto_img_{w}.png", region=(start_x, start_y, end_x - start_x, end_y - start_y)
+    )
