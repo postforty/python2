6bbdff14ccd2c5b18028ae608f43315d90f63c6f
postforty <75767073+postforty@users.noreply.github.com>
2023-09-23 12:30:06 +0900

    [UPDATE] 데이터 크롤링 및 바차트 시각화 완료

--- django/projects/mysite/pybo/views.py
+++ django/projects/mysite/pybo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+import requests
+from bs4 import BeautifulSoup
 
 
 def index(request):
@@ -7,4 +9,43 @@ def index(request):
 
 
 def chart(request):
-    return render(request, "pybo/chart.html")
+    # https://www.weather.go.kr/ > 관측기후 > 육상 > 도시별관측
+    response = requests.get("https://www.weather.go.kr/w/obs-climate/land/city-obs.do")
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    table = soup.find("table", id="weather_table")
+
+    table
+
+    data = []
+    for tr in table.find_all("tr"):
+        tds = list(tr.find_all("td"))
+
+        for td in tds:
+            if td.find("a"):
+                point = td.find("a").text
+                temperature = tds[5].text
+                humidity = tds[9].text
+
+                data.append([point, temperature, humidity])
+
+                # csv가 아닌 리스트로 만들기
+                # 수업 진행은 이 방식을 먼저 할 것!
+    selected_cities = []
+    for i in data:
+        if i[0] in ["서울", "인천", "대전", "대구", "광주", "부산", "울산"]:
+            selected_cities.append(i)
+
+    selected_cities
+
+    city_name_list = []
+    temperature_list = []
+    humidity_list = []
+    for i in selected_cities:
+        city_name_list.append(i[0])
+        temperature_list.append(float(i[1]))
+        humidity_list.append(float(i[2]))
+
+    context = {"chart_data": [city_name_list, temperature_list, humidity_list]}
+    print(context)
+    return render(request, "pybo/chart.html", context)
