cb9164a4f2602bb902e425659006b604a2f85932
postforty <75767073+postforty@users.noreply.github.com>
2024-04-29 11:44:46 +0900

    경로 가져오기

--- python2_course_example/use4_tensorflow_cat_dog/cat_dog.py
+++ python2_course_example/use4_tensorflow_cat_dog/cat_dog.py
@@ -3,14 +3,22 @@ from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 
+# 경로 가져오기
+import os
+PATH = os.path.dirname(os.path.abspath(__file__))
+print(PATH)
+
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
+
 # Load the model
-model = load_model(r"C:\Users\J\Documents\GitHub\python2\python2_course_example\use4_tensorflow_cat_dog\keras_model.h5", compile=False)
+# model = load_model(r"C:\Users\J\Documents\GitHub\python2\python2_course_example\use4_tensorflow_cat_dog\keras_model.h5", compile=False)
+model = load_model(PATH + "\keras_model.h5", compile=False)
 
 # Load the labels
-class_names = open(r"C:\Users\J\Documents\GitHub\python2\python2_course_example\use4_tensorflow_cat_dog\labels.txt", "r").readlines()
+# class_names = open(r"C:\Users\J\Documents\GitHub\python2\python2_course_example\use4_tensorflow_cat_dog\labels.txt", "r").readlines()
+class_names = open(PATH + "\labels.txt", "r").readlines()
 
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
@@ -18,7 +26,8 @@ class_names = open(r"C:\Users\J\Documents\GitHub\python2\python2_course_example\
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open(r"C:\Users\J\Documents\GitHub\python2\python2_course_example\use4_tensorflow_cat_dog\cat_182.jpg").convert("RGB")
+# image = Image.open(r"C:\Users\J\Documents\GitHub\python2\python2_course_example\use4_tensorflow_cat_dog\cat_182.jpg").convert("RGB")
+image = Image.open(PATH + "\cat_182.jpg").convert("RGB")
 
 # resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
