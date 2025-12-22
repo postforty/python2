6b6d04850fb2c2b14ffa29fe3693cf45fb4cadb6
postforty <postforty@naver.com>
2025-12-22 23:49:54 +0900

    chore: 문서 자동화 실습 테스트

--- python2_exam/use08_docx/03_certificate.py
+++ python2_exam/use08_docx/03_certificate.py
@@ -1,9 +1,9 @@
 # 구글에서 "상장 금테 고화질" 검색
 # https://mintp.tistory.com/entry/상장-양식
 # pip install python-docx
-import docx
-from docx.oxml.ns import qn
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+import docx # Word 문서 조작을 위한 주요 라이브러리
+from docx.oxml.ns import qn # XML 네임스페이스 관련 모듈
+from docx.enum.text import WD_ALIGN_PARAGRAPH # 텍스트 정렬을 위한 열거형
 
 doc = docx.Document("certificate_form.docx")
 
@@ -13,14 +13,24 @@ style = doc.styles["Normal"]
 style.font.size = docx.shared.Pt(12)
 
 para = doc.add_paragraph()
-# run = para.add_run('\n\n')
 run = para.add_run(" 제 2025-001 호\n")
-run.font.name = "HY궁서"
-run._element.rPr.rFonts.set(qn("w:eastAsia"), "HY궁서")
-run.font.size = docx.shared.Pt(20)
+
+# 폰트 설정
+# run.font.name = "HY궁서"
+# run._element.rPr.rFonts.set(qn("w:eastAsia"), "HY궁서") # 한글과 같은 동아시아 언어 폰트를 설정
+# run.font.size = docx.shared.Pt(20)
+
+def set_font(run, font_name, font_size, bold=False):
+    """텍스트 폰트, 크기, 굵기를 설정하는 함수이다."""
+    run.font.name = font_name
+    run._element.rPr.rFonts.set(qn("w:eastAsia"), font_name)
+    run.font.size = docx.shared.Pt(font_size)
+    run.bold = bold
+
+set_font(run, "HY궁서", 20)
 
 para = doc.add_paragraph()
-run = para.add_run("\n\n")
+run = para.add_run("\n")
 run = para.add_run("수 료 증")
 run.bold = True
 run.font.name = "HY궁서"
@@ -51,7 +61,7 @@ run._element.rPr.rFonts.set(qn("w:eastAsia"), "나눔고딕")
 run.font.size = docx.shared.Pt(20)
 
 para = doc.add_paragraph()
-run = para.add_run("\n\n")
+run = para.add_run("\n")
 run = para.add_run(" 위 사람은 Python 1, Python 2 교육과정을\n")
 run.font.name = "나눔고딕"
 run._element.rPr.rFonts.set(qn("w:eastAsia"), "나눔고딕")
@@ -63,12 +73,12 @@ run._element.rPr.rFonts.set(qn("w:eastAsia"), "휴먼옛체")
 run.font.size = docx.shared.Pt(20)
 
 para = doc.add_paragraph()
-run = para.add_run("\n\n\n")
+run = para.add_run("\n\n")
 run = para.add_run("코리아 IT 아카데미")
 run.font.name = "나눔고딕"
 run.bold = True
 run._element.rPr.rFonts.set(qn("w:eastAsia"), "나눔고딕")
 run.font.size = docx.shared.Pt(20)
-para.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+para.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 doc.save("certificate_result.docx")
