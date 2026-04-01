df635d8ddcec20afa8c15a8bb29485e12437ea42
postforty <postforty@naver.com>
2026-04-01 22:51:41 +0900

    chore: 불필요한 코드 삭제

--- python2_exam/use04_tensorflow_cat_dog/rename_script.ps1
+++
@@ -1,5 +0,0 @@
-Rename-Item '02_파일_목록_출력.py' '02_file_listing.py' -ErrorAction SilentlyContinue
-Rename-Item '03_넘파이_정규화_실습.py' '03_numpy_normalization.py' -ErrorAction SilentlyContinue
-Rename-Item '04_폴더_생성_및_복사.py' '04_folder_management.py' -ErrorAction SilentlyContinue
-Rename-Item 'cat_dog_taster.py' 'cat_dog_classifier.py' -ErrorAction SilentlyContinue
-Rename-Item '01_keras_modal_test.py' '01_keras_model_test.py' -ErrorAction SilentlyContinue
